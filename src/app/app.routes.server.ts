c99034fa697fa7ea38cf59f5db971bc24df00d0f
Abdul Rehman <abdulrehmansafdarwordsense2.0@gmail.com>
2025-06-05 17:44:42 +0500

    feat: Change render mode for book route from Prerender to Dynamic

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'book/:id',
+    renderMode: RenderMode.Server // Change from Prerender to Dynamic
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
